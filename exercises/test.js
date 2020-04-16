122af4c560a2a5e82054060573b3abb49805abec
Myke <mykemitrichev@gmail.com>
2020-04-16 16:25:52 +0300

    Added a virtual

--- exercises/test.js
+++ exercises/test.js
@@ -4,82 +4,53 @@ const connect = () => {
   return mongoose.connect("mongodb://127.0.0.1:27017/");
 };
 
-const student = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    favFoods: [{ type: String }],
-    info: {
-      shoesize: { type: Number },
-    },
-    school: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "school", //the school's details to be fetched from the 'school' schema
+const student = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  favFoods: [{
+    type: String
+  }],
+  info: {
+    shoesize: {
+      type: Number
     },
   },
-  { timestamps: true },
-); //student's schema
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "school", //the school's details to be fetched from the 'school' schema
+  },
+}, {
+  timestamps: true
+}, ); //student's schema
 
 const school = new mongoose.Schema({
   name: String,
   openSince: Number,
   students: Number,
   isGreat: Boolean,
-  staff: [{ type: String }],
+  staff: [{
+    type: String
+  }],
 }); //school's schema
-
+school.virtual('staffCount')
+  .get(function () {
+    return this.staff.length
+  }) //Creates a virtual method to when staffcount is called from get method to return the no. of stuff
 const School = new mongoose.model("school", school); //creates school model
 const Student = new mongoose.model("student", student); //creates student's model
 
 connect() //connects to the database and returns a promise
   .then(async (connection) => {
-    const school1 = {
-      name: "Kilimani",
-      openSince: 1980,
-      students: 750,
+    const school1 = await School.create({
+      name: 'Jkuat',
+      openSince: 1994,
+      students: 35000,
       isGreat: true,
-      staff: ["k", "v"],
-    };
-    const school2 = {
-      name: "Jirani",
-      openSince: 2000,
-      students: 700,
-      isGreat: false,
-      staff: ["k", "v", "m"],
-    };
-    const school3 = {
-      name: "Kijana",
-      openSince: 2001,
-      students: 1000,
-      isGreat: true,
-      staff: ["k", "v", "m"],
-    };
-    const schools = await School.create([school1, school2, school3]);
-    const greatSchools = await School.findOne({
-      students: { $gt: 600, $lt: 800 },
-      isGreat: true,
-    }).exec();
-    const hasStaff = await School.find({
-      staff: ["k", "v"],
+      staff: ['k', 'l', 'm']
     })
-      .sort({ openSince: -1 })
-      //.limit(2)
-      .exec();
-    console.log(greatSchools);
-    console.log(hasStaff);
-    // const school = await School.create({ name: "Jkuat" }); //creates a student
-    // const student = await Student.create({
-    //   firstName: "Myke",
-    //   favFoods: ["Rice", "Chapati"],
-    //   info: { shoesize: 42 },
-    //   school: school._id,
-    // });
-    // const match = await Student.findById(student._id).populate("school").exec();
-    //populate(field) takes the value of the reffered field and fills it in the refference field
-    //exec() executes the query.
-    //console.log(match);
+    console.log(school1.staffCount)
   })
   .catch((err) => console.error(err));
