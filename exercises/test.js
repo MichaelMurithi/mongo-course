eebd683815d968abfebd7e71024be8a1c4cabbbb
Myke <mykemitrichev@gmail.com>
2020-04-16 11:36:42 +0300

    Created advanced queries

--- exercises/test.js
+++ exercises/test.js
@@ -25,6 +25,10 @@ const student = new mongoose.Schema(
 
 const school = new mongoose.Schema({
   name: String,
+  openSince: Number,
+  students: Number,
+  isGreat: Boolean,
+  staff: [{ type: String }],
 }); //school's schema
 
 const School = new mongoose.model("school", school); //creates school model
@@ -32,16 +36,50 @@ const Student = new mongoose.model("student", student); //creates student's mode
 
 connect() //connects to the database and returns a promise
   .then(async (connection) => {
-    const school = await School.create({ name: "Jkuat" }); //creates a student
-    const student = await Student.create({
-      firstName: "Myke",
-      favFoods: ["Rice", "Chapati"],
-      info: { shoesize: 42 },
-      school: school._id,
-    });
-    const match = await Student.findById(student._id).populate("school").exec();
+    const school1 = {
+      name: "Kilimani",
+      openSince: 1980,
+      students: 750,
+      isGreat: true,
+      staff: ["k", "v"],
+    };
+    const school2 = {
+      name: "Jirani",
+      openSince: 2000,
+      students: 700,
+      isGreat: false,
+      staff: ["k", "v", "m"],
+    };
+    const school3 = {
+      name: "Kijana",
+      openSince: 2001,
+      students: 1000,
+      isGreat: true,
+      staff: ["k", "v", "m"],
+    };
+    const schools = await School.create([school1, school2, school3]);
+    const greatSchools = await School.findOne({
+      students: { $gt: 600, $lt: 800 },
+      isGreat: true,
+    }).exec();
+    const hasStaff = await School.find({
+      staff: ["k", "v"],
+    })
+      .sort({ openSince: -1 })
+      //.limit(2)
+      .exec();
+    console.log(greatSchools);
+    console.log(hasStaff);
+    // const school = await School.create({ name: "Jkuat" }); //creates a student
+    // const student = await Student.create({
+    //   firstName: "Myke",
+    //   favFoods: ["Rice", "Chapati"],
+    //   info: { shoesize: 42 },
+    //   school: school._id,
+    // });
+    // const match = await Student.findById(student._id).populate("school").exec();
     //populate(field) takes the value of the reffered field and fills it in the refference field
     //exec() executes the query.
-    console.log(match);
+    //console.log(match);
   })
   .catch((err) => console.error(err));
