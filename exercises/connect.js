6deaa6cdc00be717f32bf32b8b94a1cfae8abb3e
Myke <mykemitrichev@gmail.com>
2020-04-17 06:19:00 +0300

    Added hooks exercise

--- exercises/connect.js
+++ exercises/connect.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const connect = (url) => {
-  return mongoose.connect(url), {
-    poolSize: 10,
-  };
+const connect = url => {
+  return (
+    mongoose.connect(url),
+    {
+      poolSize: 10,
+      useNewUrlParser: true,
+    }
+  );
 };
 
 module.exports = connect;
