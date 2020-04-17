38c6d737ac997a6108644d2e5a22739ed4990be5
Myke <mykemitrichev@gmail.com>
2020-04-17 10:34:35 +0300

    API practice

--- exercises/test.js
+++ exercises/test.js
@@ -1,18 +1,46 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
+//const { urlencoded, json } = require("dev");
+
+const app = express();
+app.use(morgan("dev"));
+//app.use(urlencoded({ extended: true }));
+//app.use(json());
 
 const connect = () => {
   return mongoose.connect("mongodb://localhost:27017/");
 };
 
-const student = new mongoose.Schema({
-  firstName: String,
+app.listen(8080, () => {
+  console.log("Server Listening at port 3000...");
 });
 
-const Student = mongoose.model("student", student);
+const noteSchema = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+});
 
-connect() //Returns a promise
-  .then(async (connection) => {
-    const student = await Student.create({ firstName: "Myke" });
-    console.log(student);
+const Note = mongoose.model("note", noteSchema);
+
+connect()
+  .then(async connection => {
+    const note = await Note.create({
+      title: "My First Mongo/Express App",
+      content:
+        "This content has been fetched from mongoDB. Express server has been used to serve me.",
+    });
+    console.log(note);
   })
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err));
+
+app.get("/note", async (req, res) => {
+  const notes = await Note.find({})
+    .lean()
+    .exec();
+  res.status(200).send({ notes: notes });
+});
